afc8a7a7267cb693136b0e73d4c2ff2223bb5911
Angelica Chung <Angelica.Ramos.91@gmail.com>
2020-06-04 18:30:46 -0400

    Formated

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,28 +1,3 @@
-
-
-// Initiate global variables, arrays, objects
-// 1.	variable to store results (Array);
-// 2.	variable to store results.join("") (String); 
-// 3.	variable to store usersHouse (string);
-// 4. 	variable to store resultsDescription (string);
-// 5.	variables to store running tally for (E/I/S/N/T/F/J/P) (Number);
-// 6.	variable to store key:value pairs (Object);
-// 	a. 	Where the key is a possible result;
-// 	b.	where the value is an obj, with a key:value pair for house and description;
-// 7. 	variable to contain house crests as html element (String);
-
-// Conditionals
-// 1. 	Check option user choice, and increment respective variable (GV5);
-// 2. 	Check for higher variable from each pair, .push("") letter to resultsArray (GV1);
-
-// Display to DOM
-// 1. 	Cache element to new variable to update 
-// 		a. HOUSE;
-//		b. MBPT;
-//		c. DESCRIPTION;
-// 2.	Cache element to new variable (w/HTML) to include House Crest;
-
-
 // Create my namescape object
 const sortingHat = {};
 
@@ -171,17 +146,8 @@ sortingHat.init = function() {
 			sortingHat.mbptTraits[_val]++;
 			console.log(_val)
 		}
-
-		// $("fieldset").fadeOut(1000);
-		
-		
-		// add class .disabled attribute
-
-		
 	});
 
-	// .one()
-	// .select(), , if(has it already happened?)
 	$("button[type='submit']").on("click", function(event){
 		event.preventDefault();
 
